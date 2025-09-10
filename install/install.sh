b86b7584dd9aa390a929991dcb63a8fdd916de9c
Jhivan de Benoit <63266737+jhivandb@users.noreply.github.com>
2025-09-10 13:58:15 +0530

    Update install/install.sh
    Co-authored-by: Chalindu Kodikara <38396435+chalindukodikara@users.noreply.github.com>

--- install/install.sh
+++ install/install.sh
@@ -137,7 +137,7 @@ fi
 
 # Step 12: Add default dataplane
 if [[ -f "${SCRIPT_DIR}/add-default-dataplane.sh" ]]; then
-    bash "${SCRIPT_DIR}/quick-start/add-default-dataplane.sh" --single-cluster
+    bash "${SCRIPT_DIR}/add-default-dataplane.sh" --single-cluster
 else
     log_warning "add-default-dataplane.sh not found, skipping dataplane configuration"
 fi
