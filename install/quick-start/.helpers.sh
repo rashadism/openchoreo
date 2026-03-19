0c040da7b6d9b7dc05c9ff2e4eafc17cdc1e1e54
Nilushan Costa <nilushan@wso2.com>
2026-03-19 12:45:41 +0530

    chore: bump observability-metrics-prometheus module version and remove unused observability-plane chart.lock (#2877)
    chore: bump observability-metric-prometheus module version and remove unused observability-plane chart.lock
    
    Signed-off-by: Nilushan Costa <nilushan@wso2.com>

--- install/quick-start/.helpers.sh
+++ install/quick-start/.helpers.sh
@@ -1084,7 +1084,7 @@ install_observability_plane() {
         "--set" "fluent-bit.enabled=true"
 
     install_helm_chart "observability-metrics-prometheus" "$modules_repo/observability-metrics-prometheus" "$OBSERVABILITY_NS" "true" "true" "true" "600" \
-        "--version" "0.2.4"
+        "--version" "0.2.5"
 }
 
 # Apply ClusterWorkflowTemplates required by the workflow plane
