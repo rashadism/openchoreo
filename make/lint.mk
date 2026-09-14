0b08684c4a8e7da68dad67bcf0544dab45c733f4
Nilushan Costa <nilushan@wso2.com>
2026-09-14 15:06:57 +0530

    feat: add clusterTrait sample for PVC and allow it in componentType samples (#4712)
    Signed-off-by: Nilushan Costa <nilushan@wso2.com>

--- make/lint.mk
+++ make/lint.mk
@@ -99,7 +99,8 @@ GETTING_STARTED_FILES := \
 	$(GETTING_STARTED_DIR)/ci-workflows/ballerina-buildpack-builder.yaml \
 	$(GETTING_STARTED_DIR)/ci-workflows/dockerfile-builder.yaml \
 	$(GETTING_STARTED_DIR)/component-traits/alert-rule-trait.yaml \
-	$(GETTING_STARTED_DIR)/component-traits/hpa-trait.yaml
+	$(GETTING_STARTED_DIR)/component-traits/hpa-trait.yaml \
+	$(GETTING_STARTED_DIR)/component-traits/pvc-trait.yaml
 
 .PHONY: samples-gen
 samples-gen: ## Generate samples/getting-started/all.yaml from individual files
