783f73cfe3635e3dbf3990ae102804cef2bdeabf
Nilushan Costa <nilushan@wso2.com>
2026-09-14 14:23:13 +0530

    feat: add clusterTrait sample for HPA and allow it in componentType samples (#4705)
    Signed-off-by: Nilushan Costa <nilushan@wso2.com>

--- make/lint.mk
+++ make/lint.mk
@@ -98,7 +98,8 @@ GETTING_STARTED_FILES := \
 	$(GETTING_STARTED_DIR)/ci-workflows/gcp-buildpacks-builder.yaml \
 	$(GETTING_STARTED_DIR)/ci-workflows/ballerina-buildpack-builder.yaml \
 	$(GETTING_STARTED_DIR)/ci-workflows/dockerfile-builder.yaml \
-	$(GETTING_STARTED_DIR)/component-traits/alert-rule-trait.yaml
+	$(GETTING_STARTED_DIR)/component-traits/alert-rule-trait.yaml \
+	$(GETTING_STARTED_DIR)/component-traits/hpa-trait.yaml
 
 .PHONY: samples-gen
 samples-gen: ## Generate samples/getting-started/all.yaml from individual files
